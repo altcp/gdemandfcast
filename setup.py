05f5b02f5d13ca74a406f755db60044a4bbdfc46
altcp <alvin.tan@ttrtea.com>
2021-07-16 23:26:24 +0800

    Test Fix

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('HISTORY.rst') as history_file:
 requirements = ['Click>=7.0', 'arch>=4.15', 'pmdarima>=1.8.2', 'scikit-learn>=0.24.2', 'keras-tuner>=1.0.1', 'tensorflow>=2.4.1',
 'pandas>=1.2.5', 'xgboost>=1.4.0', 'matplotlib>=3.3.3']
 
-test_requirements = ['pytest>=3', ]
+test_requirements = ['pytest>=3.8', ]
 
 setup(
     author="Tan Chiang Pern Alvin",
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/altcp/gdemandfcast',
-    version='0.0.2',
+    version='0.1.0',
     zip_safe=False,
 )
