1cf08ee4c582870b055550c88f35a659a016987c
altcp <alvin.tan@ttrtea.com>
2021-07-16 23:31:59 +0800

    Dependencies Fix

--- setup.py
+++ setup.py
@@ -10,19 +10,20 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'arch>=4.15', 'pmdarima>=1.8.2', 'scikit-learn>=0.24.2', 'keras-tuner>=1.0.1', 'tensorflow>=2.4.1',
+requirements = ['Click>=8.0', 'arch>=4.15', 'pmdarima>=1.8.2', 'scikit-learn>=0.24.2', 'keras-tuner>=1.0.1', 'tensorflow>=2.4.1',
 'pandas>=1.2.5', 'xgboost>=1.4.0', 'matplotlib>=3.3.3']
 
-test_requirements = ['pytest>=3.8', ]
+test_requirements = ['pytest>=3.7', ]
 
 setup(
     author="Tan Chiang Pern Alvin",
     author_email='colab.tcp@gmail.com',
-    python_requires='>=3.8.5',
+    python_requires='>=3.7',
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.8.5',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="A Generic Python Package for Demand Forecasting.",
     entry_points={
