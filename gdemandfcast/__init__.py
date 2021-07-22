7c5f7efa5f09a0413d741db6df8a0e9931f8cd71
altcp <alvin.tan@ttrtea.com>
2021-07-22 16:10:21 +0800

    init

--- gdemandfcast/__init__.py
+++ gdemandfcast/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Tan Chiang Pern Alvin"""
 __email__ = 'colab.tcp@gmail.com'
 __version__ = '0.1.7'
 
-import gdemandfcast 
+from gdemandfcast import gdtsarch, armamodels, mlmodels, dlmodels, preprocessing, validation, prediction, optimization, visualization, selection, fitting, ModelTuner 
