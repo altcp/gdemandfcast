23bafee675e086ff1d8ceed450bbd7435a117df4
altcp <alvin.tan@ttrtea.com>
2021-07-22 16:54:14 +0800

    init

--- gdemandfcast/__init__.py
+++ gdemandfcast/__init__.py
@@ -4,3 +4,5 @@ __author__ = """Tan Chiang Pern Alvin"""
 __email__ = 'colab.tcp@gmail.com'
 __version__ = '0.2.1'
 __all__ = ["gdtsarch", "armamodels", "mlmodels", "dlmodels", "preprocessing", "validation", "prediction", "optimization", "visualization", "selection", "fitting"]
+
+from main import *
