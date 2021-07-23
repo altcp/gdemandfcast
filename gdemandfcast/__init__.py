1b721393fd376530ad952f9fcc51e9ad8447c0be
altcp <alvin.tan@ttrtea.com>
2021-07-23 16:29:43 +0800

    Checkpoint

--- gdemandfcast/__init__.py
+++ gdemandfcast/__init__.py
@@ -2,5 +2,5 @@
 
 __author__ = """Tan Chiang Pern Alvin"""
 __email__ = 'colab.tcp@gmail.com'
-__version__ = '0.5.6'
+__version__ = '0.5.7'
 __all__ = ["archmodels", "armamodels", "mlmodels", "dlmodels", "preprocessing", "validation", "prediction", "optimization", "visualization", "selection", "fitting", "execute"]
