6d95a0ea6fe27f7c933d1eb6f0b3b20b1727741f
altcp <alvin.tan@ttrtea.com>
2021-07-23 16:07:49 +0800

    Checkpoint

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/altcp/gdemandfcast',
-    version='0.5.5',
+    version='0.5.6',
     zip_safe=False,
 )
