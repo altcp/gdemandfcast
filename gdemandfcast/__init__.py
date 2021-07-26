351341c9f5c82ae41108ff33d6611ed3452cdf82
altcp <alvin.tan@ttrtea.com>
2021-07-26 19:34:53 +0800

    Dependencies

---
+++ gdemandfcast/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for gdemandfcast."""
+
+__author__ = """Tan Chiang Pern Alvin"""
+__email__ = "colab.tcp@gmail.com"
+__version__ = "0.6.0"
+__all__ = ["ai"]
---
+++ tests/test_example/test_hello.py
@@ -0,0 +1,12 @@
+from gdemandfcast import ai as gdf
+
+
+def import_test():
+    assert print(gdf.hello("alvin"))
+
+
+# def test_main_function():
+
+# train = '/path/to/train.xlsx'
+# test = '/path/to/test.xlsx'
+# assert print(gdf.execute(train, test, 3).frm())
