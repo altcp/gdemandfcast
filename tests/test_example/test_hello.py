687a97836db012751015e759ed849b7fcc8e4c31
altcp <alvin.tan@ttrtea.com>
2021-07-27 16:33:10 +0800

    Typo

--- tests/test_example/test_hello.py
+++ tests/test_example/test_hello.py
@@ -27,4 +27,4 @@ def test_execute():
     train = "./gdemandfcast/data/Test Data.xlsx"
     test = "./gdemandfcast/data/Test Data.xlsx"
     df = execute(train, test, 3).frm()
-    assert print(df)
+    print(df)
