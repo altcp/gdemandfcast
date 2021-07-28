2ba0124873e2851ef7c4d439bcf81813de75d0c1
altcp <alvin.tan@ttrtea.com>
2021-07-28 22:32:51 +0800

    :poop:

--- gdemandfcast/gui.py
+++ gdemandfcast/gui.py
@@ -2,29 +2,44 @@ import os
 
 import streamlit as st
 
-from gdemandfcast.ai import execute
+import gdemandfcast.ai as gdf
 
+st.title("Univariate Forecasting")
+st.subheader("Proof of Concept Demostration")
+st.markdown("***")
+st.markdown("***")
+gear = st.radio("Model Selection::", ("Compare", "Auto"))
+shift = st.selectbox("Type of Models:", ("ML", "TS", "DL"))
+lags = st.slider("Number of Previous Timsteps to Consider:", 1, 5, 3, 1)
+train = st.file_uploader("Upload Training Data: ")
+test = st.file_uploader("Uploat Testing Data: ")
+st.markdown("***")
+gear = gear.lower()
+shift = shift.lower()
 
-def create():
+if train is not None and test is not None:
 
-    st.title("Univariate Forecasting")
-    st.subheader("Proof of Concept Demostration")
-    st.markdown("***")
-    st.markdown("***")
-    gear = st.radio("Model Selection::", ("Compare", "Auto"))
-    shift = st.selectbox("Type of Models:", ("ML", "TS", "DL"))
-    lags = st.slider("Number of Previous Timsteps to Consider:", 1, 5, 3, 1)
-    train = st.file_uploader("Upload Training Data: ")
-    test = st.file_uploader("Uploat Testing Data: ")
-    st.markdown("***")
     gear = gear.lower()
     shift = shift.lower()
+    df = gdf.execute(train, test, lags, gear, shift, "fast", False).frm()
 
-    if train is not None and test is not None:
+    if shift == "auto":
+        a = 1
+    else:
+        a = 4
 
-        gear = gear.lower()
-        shift = shift.lower()
-        df = execute(train, test, lags, gear, shift, "fast", False).frm()
+    for i in range(len(df)):
+        st.line_chart(df.loc[:, i : i + a])
+        st.bar_chart(df.loc[:, i : i + a])
+
+else:
+
+    submit = st.button("Run Demo")
+    if submit:
+        st.write("Demostration Based on Seen Data.")
+        train = "./gdemandfcast/data/Train Data.xlsx"
+        test = "./gdemandfcast/data/Test Data.xlsx"
+        df = gdf.execute(train, test, 3, "auto", "ml", "fast", False).frm()
 
         if shift == "auto":
             a = 1
@@ -34,25 +49,3 @@ def create():
         for i in range(len(df)):
             st.line_chart(df.loc[:, i : i + a])
             st.bar_chart(df.loc[:, i : i + a])
-
-    else:
-
-        submit = st.button("Run Demo")
-        if submit:
-            st.write("Demostration Based on Seen Data.")
-            df = execute(train, test, lags, gear, shift, "fast", False).frm()
-
-            if shift == "auto":
-                a = 1
-            else:
-                a = 4
-
-            for i in range(len(df)):
-                st.line_chart(df.loc[:, i : i + a])
-                st.bar_chart(df.loc[:, i : i + a])
-
-    return None
-
-
-# Run Front End on Streamlit
-create()
