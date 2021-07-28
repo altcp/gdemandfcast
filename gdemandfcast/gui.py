ef9cf30a0a8cbca2761b4fc0d10d36bee8c98b7b
altcp <alvin.tan@ttrtea.com>
2021-07-28 18:00:40 +0800

    :art:

--- gdemandfcast/gui.py
+++ gdemandfcast/gui.py
@@ -1,13 +1,34 @@
+import os
+
 import streamlit as st
 
-import gdemandfcast.ai as gdf
+from gdemandfcast.ai import execute
 
 
-class frontend:
-    def create(self):
+def create():
 
-        return None
+    st.title("Univariate Forecasting")
+    st.subheader("Proof of Concept Demostration")
+
+    gear = st.radio("Model Selection::", ("Auto", "Compare"))
+    shift = st.selectbox("Type of Models:", ("ML", "TS", "AUTO"))
+    lags = st.slider("Number of Previous Timsteps to Consider:", (1, 5))
+    uploaded_train = st.file_uploader("Upload Training Data: ")
+    uploaded_test = st.file_uploader("Uploat Testing Data: ")
+
+    df = execute(train, test, lags, gear, shift, speed="fast", charts=False).frm()
+
+    if shift == "auto":
+        a = 1
+    else:
+        a = 4
+
+    for i in range(len(df)):
+        st.line_chart(df.loc[:, i : i + a])
+        st.bar_chart(df.loc[:, i : i + a])
+
+    return None
 
 
 # Run Front End on Streamlit
-frontend().create()
+create()
