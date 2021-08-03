580f11d4351d1c9bb6b4e3966a6da61dbe9f2557
altcp <alvin.tan@ttrtea.com>
2021-08-03 22:21:59 +0800

    :pencil2:

--- gdemandfcast/ai.py
+++ gdemandfcast/ai.py
@@ -283,7 +283,7 @@ class regress:
         column_names = ["Y", "ARMA", "ARIMA", "SARIMA"]
         df = pd.DataFrame(columns=column_names)
         # Remove First Element to Match Prediction
-        df["Y"] = self.test_X.iloc[1:].tolist()
+        df["Y"] = self.test_X.iloc[1:].reset_index(drop=True)
 
         for model, name in (m1, m2, m3):
             mf = model.predict(self.test_X)
